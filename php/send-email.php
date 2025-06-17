b44a26bac1efbc6f97714ad700b3f20065d8f2b7
Ankit Dey <ankitdey153@gmail.com>
2025-06-17 21:20:13 +0530

    changes

--- php/send-email.php
+++ php/send-email.php
@@ -1,59 +1,13 @@
 <?php 
 
-// // Replace this with your own email address
-// $to = 'ankitdey153@gmail.com';
-
-// function url(){
-//   return sprintf(
-//     "%s://%s",
-//     isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
-//     $_SERVER['SERVER_NAME']
-//   );
-// }
-
-// if($_POST) {
-
-//    $name = trim(stripslashes($_POST['name']));
-//    $email = trim(stripslashes($_POST['email']));
-//    $subject = trim(stripslashes($_POST['subject']));
-//    $contact_message = trim(stripslashes($_POST['message']));
-
-   
-// 	if ($subject == '') { $subject = "Contact Form Submission"; }
-
-//    // Set Message
-//    $message .= "Email from: " . $name . "<br />";
-// 	 $message .= "Email address: " . $email . "<br />";
-//    $message .= "Message: <br />";
-//    $message .= nl2br($contact_message);
-//    $message .= "<br /> ----- <br /> This email was sent from your site " . url() . " contact form. <br />";
-
-//    // Set From: header
-//    $from =  $name . " <" . $email . ">";
-
-//    // Email Headers
-// 	$headers = "From: " . $from . "\r\n";
-// 	$headers .= "Reply-To: ". $email . "\r\n";
-//  	$headers .= "MIME-Version: 1.0\r\n";
-// 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
-//    ini_set("sendmail_from", $to); // for windows server
-//    $mail = mail($to, $subject, $message, $headers);
-
-// 	if ($mail) { echo "OK"; }
-//    else { echo "Something went wrong. Please try again."; }
-
-// }
-
-
 // First, we check if the form was submitted using the POST method.
 // This prevents users from accessing the script directly.
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
    
-   echo "Hello";
+    echo "hello";
     // --- 1. SET YOUR EMAIL ADDRESS ---
     // This is the email address where you want to receive the form submissions.
-    $recipient_email = "ankitdey153@example.com"; // <-- IMPORTANT: CHANGE THIS!
+    $recipient_email = "ankitdey153@gmail.com"; // <-- IMPORTANT: CHANGE THIS!
 
     // --- 2. RETRIEVE AND SANITIZE FORM DATA ---
     // We use htmlspecialchars() and trim() to clean the data and prevent security issues.
